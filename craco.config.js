ae24d5206bbd35d7ef37c113c816e2301b3cb16d
TangYang <1553604051@qq.com>
2024-04-26 22:59:35 +0800

    设置对象云存储publicPath

--- craco.config.js
+++ craco.config.js
@@ -4,4 +4,10 @@ module.exports = {
       '/api': 'http://localhost:8000',
     },
   },
+  webpack: {
+    configure: webpackConfig => {
+      webpackConfig.output.publicPath = '	https://react-test-1311325751.cos.ap-nanjing.myqcloud.com'
+      return webpackConfig
+    },
+  },
 }
---
+++ src/store/slices/user.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+export type UserStateType = {
+  username: string
+  nikename: string
+}
+const INIT_STATE: UserStateType = { username: '', nikename: '' }
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: INIT_STATE,
+  reducers: {
+    loginReducer: (state: UserStateType, action: PayloadAction<UserStateType>) => {
+      return action.payload
+    },
+    logoutReducer: () => INIT_STATE,
+  },
+})
+
+export const { loginReducer, logoutReducer } = userSlice.actions
+
+export default userSlice.reducer
